7a9548a6ec16935ca86548fbadd59cdf75a440ce
caldrete <cesaraaldrete@gmail.com>
2021-04-26 17:34:32 -0700

    navbar created and linked to components

---
+++ client/src/views/Contacts.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+class Contacts extends Component {
+	constructor() {
+		super()
+		this.state = {}
+	}
+
+	render() {
+		
+		return (
+			<div className='Contacts'>
+				<h1>'Contacts'</h1>
+			</div>
+		)
+	}
+}
+
+export default Contacts
